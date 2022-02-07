78b7098295299c03f0e8d9284870a4fc8d257e5a
Wee <han-wei.wee@hpe.com>
2022-02-07 13:58:27 +0800

    Routes

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 // Bring back the http package
 import express from 'express';
 const app =express();
-
+const router = express.Router();
 const http = require('http');
 const hostname = "localhost";
 const port =8000;
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
+//
+router.get('/aboutus',(req,res)=>{
+    res.send("You are on the about us route")
+})
 
-app.get('/',(req,res)=>{
-    res.send(`Node and Express server is running on port ${port}`)
-});
-
-app.post('/addemployee',(req,res)=>{
-    let empName = req.body.empName;
+router.post('/addemployee',(req,res)=>{
+    let empName = req.body.empName
     let empPass = req.body.empPass;
-    res.end(`POST was successful ${empName},${empPass}`);
+    res.end(`Post was sucessful , we got ${empName} and ${empPass}`);
 });
+app.use('/',router);
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`)
